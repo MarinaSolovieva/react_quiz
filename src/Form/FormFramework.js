8e04dde7180d5da9e54997ba557f7b3dc90dfb2d
Marina Solovieva <marishasolovieva@mail.ru>
2020-05-10 22:11:06 +0300

    add function addQuestionCreator

--- src/Form/FormFramework.js
+++ src/Form/FormFramework.js
@@ -21,10 +21,8 @@ export function validate(value, validation = null) {
 
 export function validateForm(formControls) {
     let isFormValid = true;
-    for (let control in formControls) {
-        if (formControls.hasOwnProperty(control)) {
-            isFormValid = formControls[control].valid && isFormValid
-        }
-    }
+    Object.keys(formControls).forEach(name => {
+        isFormValid = formControls[name].valid && isFormValid
+    });
     return isFormValid
 }
